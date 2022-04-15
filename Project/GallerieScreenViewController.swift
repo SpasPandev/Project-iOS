59636c9631b99c7d7507c7873f4104eabd631635
SpasPandev <sppan@abv.bg>
2022-04-15 14:14:12 +0200

    Some changes

--- Project/GallerieScreenViewController.swift
+++ Project/GallerieScreenViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class GallerieScreenViewController: UIViewController {
 
-    @IBAction func openGallerie(_ sender: UIButton) {
-        
-            self.performSegue(withIdentifier: "Gallerie Screen", sender: self)
+    @IBAction func tapCustomView(_ sender: UITapGestureRecognizer) {
+        self.performSegue(withIdentifier: "Gallerie Screen", sender: self)
     }
+    
+    @IBOutlet weak var tappedCustomView: CustomView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
